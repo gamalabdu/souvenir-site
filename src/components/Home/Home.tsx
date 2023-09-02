a9c88437b3e0c750a1dda925326190c1068449e6
Gamal Abdu <abdu.s.gamal@gmail.com>
2023-09-02 14:19:32 -0400

    adding video to front page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,92 +1,84 @@
 import React, { useEffect, useRef } from 'react'
 import './styles.css'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
+import frontPageReel from '../../assets/videos/frontpagereel.mov'
 
 const Home = () => {
+	const vidref = useRef<HTMLVideoElement | null>(null)
 
-  const vidref = useRef<HTMLVideoElement | null>(null);
-
-    useEffect(() => {
-      if (vidref.current) {
-        vidref.current.play();
-      }
-    }, []);
-
-
-
+	useEffect(() => {
+		if (vidref.current) {
+			vidref.current.play()
+		}
+	}, [])
 
 	const title = 'Souvenier'
-    const description = 'Explore the works'
+	const description = 'Explore the works'
 
-    const DynamicPage = ( title : string, description : string ) => {
-      useEffect(() => {
-        document.title = title; // Update the document title
-        const metaDescription = document.querySelector('meta[name="description"]');
-        if (metaDescription) {
-          metaDescription.setAttribute('content', description); // Update the meta description tag
-        }
-      }, [title, description]);
-    }
+	const DynamicPage = (title: string, description: string) => {
+		useEffect(() => {
+			document.title = title // Update the document title
+			const metaDescription = document.querySelector('meta[name="description"]')
+			if (metaDescription) {
+				metaDescription.setAttribute('content', description) // Update the meta description tag
+			}
+		}, [title, description])
+	}
 
-    DynamicPage(title, description)
+	DynamicPage(title, description)
 
-    const fadeOut = {
-      hidden: {
-            opacity: 0,
-            y: 200,
-      },
-      show : {
-        opacity: 1,
-        y: 0,
-        transition: {
-          ease : 'easeInOut',
-          duration: 1.6,
-        }
-      },
-      exit : {
-        opacity: 0,
-        y: -200,
-        transition : {
-          ease : 'easeInOut',
-          duration: 1.6,
-        }
-      }
-    } 
+	const fadeOut = {
+		hidden: {
+			opacity: 0,
+			// y: 200,
+		},
+		show: {
+			opacity: 1,
+			// y: 0,
+			transition: {
+				ease: 'easeInOut',
+				duration: 1.6,
+			},
+		},
+		exit: {
+			opacity: 0,
+			// y: -200,
+			transition: {
+				ease: 'easeInOut',
+				duration: 1.6,
+			},
+		},
+	}
 
 	return (
-		<motion.div className='home-container'
-    initial='hidden'
-    animate='show'
-    exit='exit'
-    variants={fadeOut}
-    >
+		<motion.div
+			className='home-container'
+			initial='hidden'
+			animate='show'
+			exit='exit'
+			variants={fadeOut}>
 
-            {/* <video className='enter-video' src='https://drive.google.com/uc?id=1axprrekHXCR37zxbzWCkc5uGbL2pedg1' 
-            loop={true}
-            preload='auto'                      
-            autoPlay
-            muted
-            /> */}
+			{/* <video
+				className='enter-video'
+				src='https://drive.google.com/uc?id=1Je6dMBJKiN5srMcFS-qFfcYdlKeoA9pF'
+				loop={true}
+				ref={vidref}
+				preload='auto'
+				autoPlay
+				muted
+				playsInline
+			/> */}
 
-          <video className='enter-video' src='https://drive.google.com/uc?id=1Je6dMBJKiN5srMcFS-qFfcYdlKeoA9pF'          
-            loop={true}
-            ref={vidref}
-            preload='auto'                      
-            autoPlay
-            muted
-            playsInline
-            />
-
-
-			{/* <div style= {{ padding:"56.25% 0 0 0", position:"relative"}} >
-				<iframe
-					src='https://player.vimeo.com/video/857242977?h=8296df035b&byline=0title=0&portrait=0&sidedock=0'
-					style={{ position: "absolute", top:"0", left:"0", width:"100%", height: "100%"}}
-					frameBorder='0'
-					allow='autoplay; fullscreen; picture-in-picture'
-					allowFullScreen></iframe>
-			</div>
-			<script src='https://player.vimeo.com/api/player.js'></script> */}
+			<video
+				className='enter-video'
+				src={frontPageReel}
+				loop={true}
+				ref={vidref}
+				preload='auto'
+				autoPlay
+				muted
+				playsInline
+			/>
 		</motion.div>
 	)
 }
---
+++ src/declaration.d.ts
@@ -0,0 +1,6 @@
+declare module '*.mp4';
+declare module '*.mov';
+declare module '*.gif';
+declare module '*.jpeg';
+declare module '*.png';
+declare module '*.jpg';
